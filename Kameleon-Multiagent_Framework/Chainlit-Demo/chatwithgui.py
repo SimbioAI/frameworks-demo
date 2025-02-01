b6dc3c308c5dd60e1327629b58038ddd448c9a5e
ZouZeusA <joseph.abdo02@gmail.com>
2025-02-01 11:19:25 +0200

    Removed the .env path from the code and updated the tools usage in main.py Changed the langraph deeplearning.ai file names

--- Kameleon-Multiagent_Framework/Chainlit-Demo/chatwithgui.py
+++ Kameleon-Multiagent_Framework/Chainlit-Demo/chatwithgui.py
@@ -11,9 +11,8 @@ import chainlit as cl
 import asyncio
 
 # Load environment variables
-from dotenv import load_dotenv
-
-load_dotenv("F:\\Social AI\\multiagent-framework\\.venv\\.env")
+from dotenv import load_dotenv, find_dotenv    
+load_dotenv(find_dotenv())
 
 #* Callback
 class MyCustomChainAsyncHandler(AsyncCallbackHandler):
--- Kameleon-Multiagent_Framework/main.py
+++ Kameleon-Multiagent_Framework/main.py
@@ -1,13 +1,12 @@
-from typing import List
-from typing_extensions import Annotated
+from typing import List, Annotated
 from pydantic import BaseModel, Field
 from langchain_core.tools import tool, InjectedToolArg, StructuredTool, ToolException
 from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
 
 #* Load environment variables
-from dotenv import load_dotenv
-
-load_dotenv("F:\\Social AI\\multiagent-framework\\.venv\\.env")
+from dotenv import load_dotenv, find_dotenv    
+load_dotenv(find_dotenv())
 
 #* Load Chat Model
 model = ChatOpenAI(model="gpt-4o-mini", 
@@ -26,22 +25,46 @@ model_with_structure = model.with_structured_output(ResponseFormatter)
 
 
 #* Tool creation
+# can add chain to inject tool argument without the llm knowing about it
 @tool
-def my_tool(a: int, b: int) -> int:
-    """Multiply two numbers."""
+def multiply(a: Annotated[int,"first number"], b: Annotated[int,"second number"]) -> int:
+    """
+    Multiply two numbers.
+    """
     return a * b
 
-@tool
-def my_2_tool(a: int, b: int) -> int:
-    """Add two numbers."""
+class AddInput(BaseModel):
+    a: int = Field(description="first number")
+    b: int = Field(description="second number")
+
+def add(a: int, b: int) -> int:
     return a + b
 
-tools = [my_tool, my_2_tool]
+def _handle_error(error: ToolException) -> str:
+    return f"The following errors occurred during tool execution: `{error.args[0]}`"
+
+add = StructuredTool.from_function(
+    func=add,
+    name="add",
+    description="Add two numbers.",
+    args_schema=AddInput,
+    return_direct=True,
+    handle_tool_error=_handle_error,
+)
+
+tools = [multiply, add]
+tools_map = {tool.name.lower(): tool for tool in tools}
 # Tool binding
 model_with_tools = model.bind_tools(tools)
 
 result = model_with_tools.invoke("Hello world!")
 
-result_1 = model_with_tools.invoke("What is 2 times 3?")
+messages = [HumanMessage(content="What is 2 plus 3? What is 2 times 3?")]
+ai_message = model_with_tools.invoke(messages)
+messages.append(ai_message)
+for tool_call in ai_message.tool_calls:
+    selected_tool = tools_map[tool_call["name"].lower()]
+    tool_msg = selected_tool.invoke(tool_call)
+    messages.append(tool_msg)
 
-result_2 = model_with_tools.invoke("What is 2 plus 3?")
+model_with_tools.invoke(messages)
